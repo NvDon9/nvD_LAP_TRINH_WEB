45430ca572bf21a545230d7d744cf338b8464179
NvDon9 <vandong2006bg@gmail.com>
2026-08-22 16:18:03 +0700

    Delete nvD_Day2 directory

--- nvD_Day2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace nvD_Day2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
