835caef7ebd5e14a7892159d14fbdccca9235bf7
DongKiemEm <vandong2006bg@dong2006bggmail.006bggmailcom>
2026-08-22 16:17:14 +0700

    update code

---
+++ nvD_Day2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nvD_Day2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
