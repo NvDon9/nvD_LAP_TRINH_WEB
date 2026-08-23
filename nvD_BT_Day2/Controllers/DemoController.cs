feb632af2756601a5c58b43ad530b258ffba3694
DongKiemEm <vandong2006bg@dong2006bggmail.006bggmailcom>
2026-08-23 15:01:46 +0700

    update code

--- nvD_BT_Day2/Controllers/DemoController.cs
+++ nvD_BT_Day2/Controllers/DemoController.cs
@@ -2,8 +2,7 @@
 
 namespace nvD_BT_Day2.Controllers
 {
-    [Route("Demo")]
-    [Route("DemoController")]
+  
     public class DemoController : Controller
     {
         public IActionResult Index()
