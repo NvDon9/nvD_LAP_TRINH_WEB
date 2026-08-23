51d3627c4669b0867219da8e8b27c425cfefa860
DongKiemEm <vandong2006bg@dong2006bggmail.006bggmailcom>
2026-08-23 14:54:59 +0700

    update code

---
+++ nvD_BT_Day2/Controllers/DemoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nvD_BT_Day2.Controllers
+{
+    [Route("Demo")]
+    [Route("DemoController")]
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
